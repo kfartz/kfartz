09a003eefeb03b694ef93f97b9478f3bcf395731
chrontax <chrontax@proton.me>
2025-12-15 16:01:10 +0100

    feat(dev-env): improve setup script
    Automatically install automake 1.16.1 and move all build artefacts from
    cif_api to .deps.

--- setup.sh
+++ setup.sh
@@ -1,35 +1,41 @@
 #!/bin/bash
 
-# Install libcif 
-CIFLIB_DIR=cif_api
-if [[ ! -d $CIFLIB_DIR ]]; then
-    git submodules update --init --recursive
-    pushd $CIFLIB_DIR
-
-    ./configure && make
-
-    popd
-fi
-
-export LD_LIBRARY_PATH="$PWD/$CIFLIB_DIR/src/.lib:$LD_LIBRARY_PATH"
-
-# Install bun 
-
-BUNDIR=.bun
+DEPDIR=$PWD/.deps
 BUNVER=1.3.4
+AMVER=1.16.1
 
-if [[ ! -d $BUNDIR ]]; then
-    mkdir $BUNDIR
+if [[ ! -d $DEPDIR ]]; then
+  mkdir -p $DEPDIR/bin
 
-    pushd $BUNDIR
-    
-    curl -O0 -L https://github.com/oven-sh/bun/releases/download/bun-v$BUNVER/bun-linux-x64-baseline.zip
-    
-    unzip bun-linux-x64-baseline.zip
+  pushd $DEPDIR
 
-    rm bun-linux-x64-baseline.zip
-    
-    popd
+  curl -O0 -L https://github.com/oven-sh/bun/releases/download/bun-v$BUNVER/bun-linux-x64-baseline.zip
+  unzip bun-linux-x64-baseline.zip && rm bun-linux-x64-baseline.zip
+  ln bun-linux-x64-baseline/bun bin/bun
+
+  curl -O0 -L https://ftp.gnu.org/gnu/automake/automake-$AMVER.tar.gz
+  tar xf automake-$AMVER.tar.gz && rm automake-$AMVER.tar.gz
+
+  pushd automake-$AMVER
+
+  ./configure --prefix=$PWD/../ && make && make install
+
+  popd
+
+  popd
 fi
 
-export PATH="$PWD/$BUNDIR/bun-linux-x64-baseline:$PATH"
+export PATH="$DEPDIR/bin:$PATH"
+# Install libcif
+CIFLIB_DIR=$PWD/cif_api
+if [[ ! -d $DEPDIR/lib ]]; then
+  git submodule update --init --recursive
+
+  mkdir $DEPDIR/cif_api_build && pushd $DEPDIR/cif_api_build
+
+  $CIFLIB_DIR/configure --prefix=$DEPDIR && make && make install
+
+  popd
+fi
+
+export LD_LIBRARY_PATH="$DEPDIR/lib:$LD_LIBRARY_PATH"
