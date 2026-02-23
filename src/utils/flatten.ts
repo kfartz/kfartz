cbc43fb14a1fb42ad8d43dcea80544af3a4784fa
Marcin Chiliński <marcin.chilinski.mobile@gmail.com>
2026-02-23 18:07:59 +0100

    fix(data-display): refinement's previous_refinements display issues

--- src/utils/flatten.ts
+++ src/utils/flatten.ts
@@ -1,27 +1,10 @@
-type Primitive = string | number | boolean | bigint | symbol | null | undefined;
-
-// biome-ignore lint: suspicious/noExplicitAny
-type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends (
-  k: infer I,
-) => void
-  ? I
-  : never;
-
-type FlattenedObject<T> = UnionToIntersection<Flatten<T>>;
-
-type Flatten<T, Prefix extends string = ""> = {
-  [K in keyof T & string]: T[K] extends Primitive | readonly unknown[]
-    ? { [P in `${Prefix}${K}`]: T[K] }
-    : T[K] extends Record<string, unknown>
-      ? Flatten<T[K], `${Prefix}${K}.`>
-      : { [P in `${Prefix}${K}`]: T[K] };
-}[keyof T & string];
+type TSquashed = Record<string, string | boolean | number | Array<unknown>>;
 
 export function flattenObject<T extends object>(
   obj: T,
-  ret: Partial<FlattenedObject<T>> = {},
+  ret: TSquashed = {},
   currKey = "",
-): FlattenedObject<T> {
+): TSquashed {
   for (const key of Object.keys(obj)) {
     const value = (obj as Record<string, unknown>)[key];
     const newKey: string = !currKey ? key : `${currKey}.${key}`;
@@ -29,11 +12,9 @@ export function flattenObject<T extends object>(
     if (value !== null && typeof value === "object" && !Array.isArray(value)) {
       flattenObject((obj as Record<string, object>)[key], ret, newKey);
     } else {
-      (ret as Record<string, object>)[newKey] = (obj as Record<string, object>)[
-        key
-      ];
+      (ret as TSquashed)[newKey] = (obj as TSquashed)[key];
     }
   }
 
-  return ret as FlattenedObject<T>;
+  return ret;
 }
